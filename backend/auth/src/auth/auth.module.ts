1f19c24821f51b3065ce74d555257dd722c0a840
Darina Malacova <dmalacov@student.codam.nl>
2024-03-20 01:59:20 +0100

    kafka client added to auth module

--- backend/auth/src/auth/auth.module.ts
+++ backend/auth/src/auth/auth.module.ts
@@ -9,9 +9,25 @@ import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './utils/jwt-strategy';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
+    ClientsModule.register([
+      {
+        name: 'STATS_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'stats',
+            brokers: ['kafka:29092'],
+          },
+          consumer: {
+            groupId: 'stats-consumer',
+          },
+        },
+      },
+    ]),
     PassportModule.register({ session: false }),
     JwtModule.registerAsync({
       inject: [ConfigService],
--- backend/auth/src/auth/auth.service.ts
+++ backend/auth/src/auth/auth.service.ts
@@ -1,15 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ValidateUserDto } from 'src/user/dto/validate-user-dto';
 import { User } from 'src/user/user.entity';
 import { UserService } from 'src/user/user.service';
 import { JwtPayloadDto } from './dto/jwt-payload-dto';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly userService: UserService,
     private readonly jwtService: JwtService,
+    @Inject('STATS_SERVICE') private readonly statsClient: ClientKafka,
   ) {}
   async validateUser({ intra_login }: ValidateUserDto): Promise<User> {
     const user = await this.userService.getUserByIntraLogin(intra_login);
@@ -21,6 +23,12 @@ export class AuthService {
     const newUser = await this.userService.createUser({
       intra_login: intra_login,
       user_name: intra_login,
+    });
+    // broadcast new user creation to profile and chat
+    this.statsClient.emit('new_user', {
+      user_id: newUser.user_id,
+      intra_login: newUser.intra_login,
+      user_name: newUser.user_name,
     });
     return newUser;
     // or should we return null in some cases?
