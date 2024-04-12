c1a4bc228ca31a0cfcd3a4cbd56028156ed6a605
Orlando Hengelmolen <ohengelm@f0r4s21.codam.nl>
2024-04-12 21:56:29 +0200

    [EDIT] improved visual

--- frontend/src/pages/Game.js
+++ frontend/src/pages/Game.js
@@ -3,6 +3,8 @@ import { styled } from 'styled-components';
 import React, { useEffect } from 'react';
 import io from 'socket.io-client';
 
+import './game.css';
+
 const GameStyle = styled.div`
 	// width: 100px;
 	height: 700px;
@@ -15,6 +17,11 @@ const GameStyle = styled.div`
 	background: #066d0d;
 `
 
+// function test()
+// {
+
+// }
+
 function Game()
 {
 	//   const draw = (context,count) => {
@@ -23,15 +30,15 @@ function Game()
 	//     const d = count % 800
 	//     context.fillRect(10 +d , 10  , 100 , 100)
 	//   }
-	const draw = (context, count) =>
-	{
-		context.clearRect(0, 0, context.canvas.width, context.canvas.height)
-		context.fillStyle = 'white'
-		const d = count % 800
-		context.beginPath();
-		context.arc(10 + d, 10, 50, 0, Math.PI * 2, false);
-		context.fill();
-	};
+	// const draw = (context, count) =>
+	// {
+	// 	context.clearRect(0, 0, context.canvas.width, context.canvas.height)
+	// 	context.fillStyle = 'white'
+	// 	const d = count % 800
+	// 	context.beginPath();
+	// 	context.arc(10 + d, 10, 50, 0, Math.PI * 2, false);
+	// 	context.fill();
+	// };
 
 	useEffect(() =>
 	{
@@ -69,10 +76,89 @@ function Game()
 			console.error("Socket.IO error: ", message);
 		});
 
+		
 		socket.on("pong", (message) =>
 		{
-			console.log("received: ", message);
-		})
+			renderBackground();
+			renderPong(message);
+			// context.fillStyle = "rgba(23, 23, 23, 0.99)";
+			// context.fillRect(0, 0, gameDiv.width, gameDiv.height);
+			// console.log("received: ", message);
+			// renderHUD(message);
+		});
+		
+		
+		handleResizeEvent();
+
+		function handleResizeEvent(event)
+		{
+			const gameDiv = document.getElementById("game");
+			adjustSize(gameDiv, document.getElementById("gameBackground"));
+			adjustSize(gameDiv, document.getElementById("gamePongCanvas"));
+			
+		}
+		
+		function	adjustSize(gameDiv, element)
+		{
+			element.width = gameDiv.offsetWidth;
+			element.height = gameDiv.offsetHeight;
+		}
+
+		function renderBackground()
+		{
+			const Background = document.getElementById("gameBackground");
+			const context = Background.getContext('2d');
+			
+			fillContext(Background, context, "rgba(23, 23, 23, 0.99)");
+		}
+		
+		function renderPong(message)
+		{
+			const element = document.getElementById("gamePongCanvas");
+			const context = element.getContext("2d");
+			
+			console.log(message);
+			const data = JSON.parse(message);
+			console.log("data player1", data.Player1)
+			clearContext(element, context);
+			addPaddle(element, context, data.Player1);
+			addPaddle(element, context, data.Player2);
+			addBall(element, context, data.Ball);
+		}
+
+		function	addPaddle(element, context, player)
+		{
+			context.fillStyle = "orange";
+
+			player.posX = (player.posX - (player.width / 2)) * element.width;
+			player.posY = (player.posY - (player.height / 2)) * element.height;
+		
+			context.fillRect(player.posX, player.posY, player.width * element.width, player.height * element.height);
+		}
+
+		function	addBall(element, context, ball)
+		{
+
+		}
+
+		function renderHUD(message)
+		{
+			const msg = JSON.parse(message);
+
+
+			console.log("I did a thing");
+		}
+
+		function	fillContext(element, context, color)
+		{
+			context.fillStyle = color;
+			context.fillRect(0, 0, element.width, element.height);
+		}
+		
+		function	clearContext(element, context)
+		{
+			context.clearRect(0, 0, element.width, element.height);
+		}
 
 		const HandleKeyEvent = (event) =>
 		{
@@ -107,12 +193,17 @@ function Game()
 
 	return (
 		<>
-			<GameStyle>
-				<div className="canvas-container">
+			{/* <GameStyle> */}
+				<div id="game">
+					<canvas id="gameBackground"></canvas>
+					<canvas id="gamePongCanvas"></canvas>
+				</div>
+				{/* <script src="GameConnect.js"></script> */}
+				{/* <div className="canvas-container"> */}
 					{/* <GameConnect></GameConnect> */}
 					{/* <Canvas draw={draw} width="800" height="400" /> */}
-				</div>
-			</GameStyle>
+				{/* </div> */}
+			{/* </GameStyle> */}
 		</>
 	);
 }
