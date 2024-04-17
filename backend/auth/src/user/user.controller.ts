de40bd5fdb95df7e2837c864990d7f1a2a146703
Darina Malacova <dmalacov@student.codam.nl>
2024-04-17 15:07:33 +0200

    removed superfluous functions from auth/user

--- backend/auth/src/user/user.controller.ts
+++ backend/auth/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user-dto';
 import { User } from './user.entity';
 import { UserService } from './user.service';
@@ -10,18 +10,5 @@ export class UserController {
   @Post()
   createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.userService.createUser(createUserDto);
-  }
-
-  @Get('/:id')
-  getUserById(@Param('id') id: string): Promise<User> {
-    return this.userService.getUserById(id);
-  }
-
-  @Patch('/:id/user_name')
-  changeUserName(
-    @Param('id') id: string,
-    @Body('user_name') user_name: string,
-  ): Promise<User> {
-    return this.userService.changeUserName(id, user_name);
   }
 }
