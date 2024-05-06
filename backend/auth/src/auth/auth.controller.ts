4a291540416826de52fadc460bec735fd2c231fa
Darina Malacova <dmalacov@student.codam.nl>
2024-05-06 21:49:45 +0200

    logout method added; login renamed for clarity

--- backend/auth/src/auth/auth.controller.ts
+++ backend/auth/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { FourtyTwoAuthGuard } from './utils/fourty-two-auth-guard';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './utils/jwt-auth-guard';
@@ -18,14 +18,20 @@ export class AuthController {
   @UseGuards(FourtyTwoAuthGuard)
   @Get('42/redirect')
   handleRedirect(@Req() req, @Res() resp: Response): void {
-    const cookie = this.authService.login(req.user);
+    const cookie = this.authService.getCookieForLogin(req.user);
     resp.setHeader('Set-Cookie', cookie);
     return resp.redirect(302, this.configService.get('DASHBOARD_URL'));
   }
 
   // route auth/42/refresh
 
-  // route auth/logout
+  @UseGuards(JwtAuthGuard)
+  @Post('logout')
+  logout(@Req() req, @Res() resp: Response): Response {
+    const cookie = this.authService.getCookieForLogout();
+    resp.setHeader('Set-Cookie', cookie);
+    return resp.sendStatus(200);
+  }
 
   @UseGuards(JwtAuthGuard)
   @Get('profile')
@@ -35,6 +41,9 @@ export class AuthController {
 }
 
 /**
- * This is how the cookie will look like:
+ * This is how the cookie will look like after login:
  * Authentication=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5NzgzYTUxNy05ZDgzLTRhMjItOWEwZi01NTUxZGYyYTZmYWIiLCJ1c2VyX25hbWUiOiJkbWFsYWNvdiIsImludHJhX2xvZ2luIjoiZG1hbGFjb3YiLCJpYXQiOjE3MTQyMjgyMzksImV4cCI6MTcxNDIzMTgzOX0.vqq-Ua6EUe0PaJJEmYQkHvflKL7pA_9K0z03is5f0Lo; Refresh=101c1fab53cb354d0605cc126bd65124d4f8e12fc7759ca6f27f0cf9341ae15e HttpOnly; Path=/; secure=true; Max-Age=3600
+ *
+ * and after logout:
+ * Authentication=; Refresh=; HttpOnly; Path=/; secure=true; Max-Age=0
  */
