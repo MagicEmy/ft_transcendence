ae435149f19da4b3c1d96b7b53317469964f95f5
Darina Malacova <dmalacov@student.codam.nl>
2024-05-13 22:51:42 +0200

    JwtAuthGuard split into JwtAccessAuthGuard and JwtRefreshAuthGuard

--- backend/auth/src/auth/auth.controller.ts
+++ backend/auth/src/auth/auth.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { FourtyTwoAuthGuard } from './utils/fourty-two-auth-guard';
 import { AuthService } from './auth.service';
-import { JwtAuthGuard } from './utils/jwt-auth-guard';
+import { JwtAccessAuthGuard } from './utils/jwt-access-auth-guard';
 import { Response } from 'express';
 import { ConfigService } from '@nestjs/config';
+import { JwtRefreshAuthGuard } from './utils/jwt-refresh-auth-guard';
 
 @Controller('auth')
 export class AuthController {
@@ -18,32 +19,94 @@ export class AuthController {
   @UseGuards(FourtyTwoAuthGuard)
   @Get('42/redirect')
   handleRedirect(@Req() req, @Res() resp: Response): void {
-    const cookie = this.authService.getCookieForLogin(req.user);
-    resp.setHeader('Set-Cookie', cookie);
+    const { jwtAccessToken, jwtRefreshToken } = this.authService.login(
+      req.user,
+    );
+    // setting the jwt tokens in cookies
+    resp.cookie('Authentication', jwtAccessToken, {
+      path: '/',
+      secure: true,
+      expires: new Date(
+        Date.now() +
+          Number(this.configService.get('JWT_ACCESS_EXPIRATION_TIME')),
+      ),
+      httpOnly: false,
+    });
+    resp.cookie('Refresh', jwtRefreshToken, {
+      path: '/',
+      secure: true,
+      expires: new Date(
+        Date.now() +
+          Number(this.configService.get('JWT_REFRESH_EXPIRATION_TIME')),
+      ),
+      httpOnly: true,
+    });
     return resp.redirect(302, this.configService.get('DASHBOARD_URL'));
   }
 
-  // route auth/42/refresh
+  @UseGuards(JwtRefreshAuthGuard)
+  //   @Post('/refresh')
+  @Get('/refresh') // this is just for testing from browser, otherwise this method should be POST
+  refreshJwtToken(@Req() req, @Res() resp: Response) {
+    // generating new set of jwt tokens
+    const jwtAccessToken = this.authService.generateJwtAccessToken({
+      sub: req.user.user_id,
+      user_name: req.user_name,
+      intra_login: req.intra_login,
+    });
+    const jwtRefreshToken = this.authService.generateJwtRefreshToken(
+      req.user.user_id,
+    );
+    // setting the jwt tokens in cookies
+    resp.cookie('Authentication', jwtAccessToken, {
+      path: '/',
+      secure: true,
+      expires: new Date(
+        Date.now() +
+          Number(this.configService.get('JWT_ACCESS_EXPIRATION_TIME')),
+      ),
+      httpOnly: false,
+    });
+    resp.cookie('Refresh', jwtRefreshToken, {
+      path: '/',
+      secure: true,
+      expires: new Date(
+        Date.now() +
+          Number(this.configService.get('JWT_REFRESH_EXPIRATION_TIME')),
+      ),
+      httpOnly: true,
+    });
+    resp.send();
+  }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAccessAuthGuard)
   @Post('logout')
   logout(@Req() req, @Res() resp: Response): Response {
-    const cookie = this.authService.getCookieForLogout();
-    resp.setHeader('Set-Cookie', cookie);
+    resp.clearCookie('Authentication');
+    resp.clearCookie('Refresh');
     return resp.sendStatus(200);
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAccessAuthGuard)
   @Get('profile')
   getProfile(@Req() req) {
     return req.user;
   }
+
+  // FOR TESTING TOKEN REFRESHING
+  @Get('/simulate')
+  async simulate(@Req() req, @Res() resp: Response) {
+    resp.redirect('http://localhost:3003/auth/refresh');
+  }
 }
 
 /**
- * This is how the cookie will look like after login:
- * Authentication=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5NzgzYTUxNy05ZDgzLTRhMjItOWEwZi01NTUxZGYyYTZmYWIiLCJ1c2VyX25hbWUiOiJkbWFsYWNvdiIsImludHJhX2xvZ2luIjoiZG1hbGFjb3YiLCJpYXQiOjE3MTQyMjgyMzksImV4cCI6MTcxNDIzMTgzOX0.vqq-Ua6EUe0PaJJEmYQkHvflKL7pA_9K0z03is5f0Lo; Refresh=101c1fab53cb354d0605cc126bd65124d4f8e12fc7759ca6f27f0cf9341ae15e; Path=/; secure=true; Max-Age=3600
+ * This is how the TWO cookies will look like after login:
+ * Authentication=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjM2RiODgxYy1mZTI3LTQzMDAtYjVmOC0zNjEzNGQ4OTE4ZTQiLCJ1c2VyX25hbWUiOiJ3aWxkX3RoaW5nIiwiaW50cmFfbG9naW4iOiJkbWFsYWNvdiIsImlhdCI6MTcxNTYzMTUyNywiZXhwIjoxNzE1NjMxNTMwfQ.vpseAjt1nklrZgYXIHKfAE_2Y-qwbLUprEnC-ADauuU; Path=/; Expires=Mon, 13 May 2024 20:18:51 GMT; Secure
+ *
+ * Refresh=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjM2RiODgxYy1mZTI3LTQzMDAtYjVmOC0zNjEzNGQ4OTE4ZTQiLCJpYXQiOjE3MTU2MzE1MjcsImV4cCI6MTcxNTYzMTYxM30.0ocTzIgGOM7BzXBEwD3DOxAFiF-3popkp9_t6aPyMIE; Path=/; Expires=Mon, 13 May 2024 20:20:14 GMT; HttpOnly; Secure
  *
  * and after logout:
- * Authentication=; Refresh=; Path=/; secure=true; Max-Age=0
+ * Authentication=; Path=/; secure=true; Max-Age=0
+ * Refresh=; Path=/; secure=true; Max-Age=0
  */
