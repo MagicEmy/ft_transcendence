f49ef64df97860a26b8d3927d2b8d9d192af7d78
Darina Malacova <dmalacov@student.codam.nl>
2024-06-04 21:24:39 +0200

    moved all tfa-related code to the tfa folder

--- backend/auth/src/tfa/tfa.repository.ts
+++ backend/auth/src/tfa/tfa.repository.ts
@@ -1,12 +1,10 @@
 import { InjectRepository } from '@nestjs/typeorm';
-import { CreareTFADto } from './dto/create-tfa-dto';
+import { CreareTFADto } from '../user/dto/create-tfa-dto';
 import { Repository } from 'typeorm';
 import { Tfa } from './tfa.entity';
 
 export class TfaRepository extends Repository<Tfa> {
-  constructor(
-    @InjectRepository(Tfa) private tfaRepository: Repository<Tfa>,
-  ) {
+  constructor(@InjectRepository(Tfa) private tfaRepository: Repository<Tfa>) {
     super(
       tfaRepository.target,
       tfaRepository.manager,
@@ -14,9 +12,9 @@ export class TfaRepository extends Repository<Tfa> {
     );
   }
   async addTwoFactorAuthentication(createTfaDto: CreareTFADto): Promise<Tfa> {
-      let tfa = await this.findOne({
-          where: { user_id: createTfaDto.user_id }
-      });
+    let tfa = await this.findOne({
+      where: { user_id: createTfaDto.user_id },
+    });
     if (tfa) {
       tfa.is_enabled = true;
       tfa.secret = createTfaDto.secret;
@@ -29,28 +27,28 @@ export class TfaRepository extends Repository<Tfa> {
   }
 
   async getTwoFactorAuthenticationSecret(user_id: string): Promise<string> {
-      const tfa = await this.findOne({
-          where: { user_id: user_id }
-      });
+    const tfa = await this.findOne({
+      where: { user_id: user_id },
+    });
     return tfa.secret;
   }
 
   async enableTwoFactorAuthentication(user_id: string): Promise<Tfa> {
-      const tfa = await this.findOne({ where: { user_id: user_id } });
+    const tfa = await this.findOne({ where: { user_id: user_id } });
     tfa.is_enabled = true;
     this.save(tfa);
     return tfa;
   }
 
   async disableTwoFactorAuthentication(user_id: string): Promise<Tfa> {
-      const tfa = await this.findOne({ where: { user_id: user_id } });
+    const tfa = await this.findOne({ where: { user_id: user_id } });
     tfa.is_enabled = false;
     this.save(tfa);
     return tfa;
   }
 
   async isTwoFactorAuthenticationEnabled(user_id: string): Promise<boolean> {
-      const tfa = await this.findOne({ where: { user_id: user_id } });
+    const tfa = await this.findOne({ where: { user_id: user_id } });
     return tfa.is_enabled;
   }
 }
