ff71a46a71b311931047c1a57d606d1cc395ef4c
Darina Malacova <dmalacov@student.codam.nl>
2024-06-09 16:41:28 +0200

    [EDIT] 42 AuthGuard now redirects to homepage when authentication fails

--- backend/auth/src/auth/utils/fourty-two-auth-guard.ts
+++ backend/auth/src/auth/utils/fourty-two-auth-guard.ts
@@ -1,5 +1,6 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { FoundException } from './found-exception';
 
 @Injectable()
 export class FourtyTwoAuthGuard extends AuthGuard('42') {
@@ -8,9 +9,17 @@ export class FourtyTwoAuthGuard extends AuthGuard('42') {
       const activate = (await super.canActivate(context)) as boolean;
       const request = super.getRequest(context);
       await super.logIn(request);
-      return activate;
+      if (!activate) {
+        const resp = context.switchToHttp().getResponse();
+        resp.setHeader('location', 'http://localhost:3000/?status=forbidden');
+        console.log('just set the header', resp.header);
+        throw new FoundException('Redirecting you to login...');
+      } else {
+        return true;
+      }
     } catch (error) {
       console.log(error);
+      throw error;
     }
   }
 }
