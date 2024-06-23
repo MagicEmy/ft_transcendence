8d9583f6935125c8bef75768e7f1ca8a81553808
Emanuela <emanuela.licameli@gmail.com>
2024-06-23 19:59:53 +0200

    removed App.css and added space between gameHistory items

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -1,6 +1,5 @@
 
-import "./Chat.css";
-import Chat from "./ChatPage";
+import ChatPage from "./ChatPage";
 import React, { useState, FC } from "react"
 import { ChatContext, socket } from "../../context/ChatContext";
 import { ChatContextType, RoomDto, ChatUserDto, RoomShowDto, RoomUserDto,UserDto, MessageRoomDto } from "../../types/chat.dto";
