63ca3d5cc7d9891211027ce8806378c584ec8cca
Debora Monfrini <dmonfrin@f1r3s9.codam.nl>
2024-07-14 16:25:23 +0200

    hetting host from env

--- frontend/src/context/ChatContext.tsx
+++ frontend/src/context/ChatContext.tsx
@@ -1,7 +1,8 @@
 import { io, Socket } from "socket.io-client";
 import React from "react";
 import { ChatContextType } from "../types/chat.dto";
-const SOCKET_URL = "http://localhost:3005";
+const host : string = process.env.REACH_APP_HOST;
+const SOCKET_URL = `http://${host}:3005`;
 export const socket: Socket = io(SOCKET_URL);
 // app context
 export const ChatContext = React.createContext<ChatContextType | undefined>(undefined);
