f742a1056509d741218104aa4d92aae2cbfaae7a
Emanuela <emanuela.licameli@gmail.com>
2024-08-10 11:55:57 +0200

    new login page image in background

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,16 +1,29 @@
-import PageContent from '../components/PageContent';
-import { useNewUserStatus } from '../hooks';
-import smash from "../assets/SmashN.png";
+import PageContent from "../components/PageContent";
+import { useNewUserStatus } from "../hooks";
+import smash from "../assets/DataSunBig.png";
 
 export const Dashboard = () => {
-	useNewUserStatus('online');
+  useNewUserStatus("online");
 
   return (
     <>
       <PageContent title="Welcome!">
         <br />
         <p>Hit Play! Chat Away - Pong Awaits!</p>
-        <img src={smash} alt="Logo" style={{ marginLeft: '300px', opacity: 0.5 }} />
+        <img
+          src={smash}
+          alt="Logo"
+          style={{
+            opacity: 0.2,
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            zIndex: -1,
+          }}
+        />
       </PageContent>
     </>
   );
